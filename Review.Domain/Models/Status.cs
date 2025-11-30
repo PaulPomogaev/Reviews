c13e1781030eda4973f1c25f52c03a0c0d9d3ea9
Paul Pomogaev <paulslock1@gmail.com>
2025-11-30 21:32:33 +0400

    добавил возможность того, чтобы статус Enum отображался текстом, вход по https://localhost:7274/swagger/index.html реализовал случайно в первой задаче в рамках рефакторинга.

--- Review.Domain/Models/Status.cs
+++ Review.Domain/Models/Status.cs
@@ -1,9 +1,11 @@
 ﻿
+using System.Text.Json.Serialization;
+
 namespace Review.Domain.Models
 {
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public enum Status
     {
-
         None = 0,
         Actual = 1,
         Deleted = 2
